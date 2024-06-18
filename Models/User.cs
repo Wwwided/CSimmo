461f01359788bbe52eff00a6aa52dc780785e3c3
unknown <widedibcegosgmail.com>
2024-06-18 09:59:22 +0200

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+namespace BACKTESTIMMO.Models
+{
+    public class User
+    {
+    	public int Id { get; set; }
+    	public string Name { get; set; }
+    	public string Email { get; set; }
+    	public DateTime CreatedAt { get; set; }
+    	public User()
+    	{
+    		CreatedAt = DateTime.Now;
+    	}
+    }
+}
+
